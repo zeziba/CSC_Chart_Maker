ab164704b9b143441ade936fee0f2d62acd02c60
zeziba <zeziba@mail.com>
2016-11-18 10:31:44 -0700

    Update splice_data.py
    Fixed chunk size

--- Scripts/splice_data.py
+++ Scripts/splice_data.py
@@ -19,7 +19,8 @@ with open("LOG0_new.CSV", "r") as old_file:
     count = 0
     data = pandas.read_csv(old_file)
     dMax = int(data.max(numeric_only=True)[0])
-    ranges = [int(dMax / i) for i in range(1, 30 if len(sys.argv) <= 1 else int(list(sys.argv)[1]) + 1)][::-1]
+    cSize = 30 if len(sys.argv) <= 1 else int(list(sys.argv)[1])
+    ranges = [int(dMax / cSize) * i for i in range(1, 30 if len(sys.argv) <= 1 else int(list(sys.argv)[1]) + 1)][::-1]
     for index, i in enumerate(ranges):
         if index > 0:
             data[ranges[index - 1]: i - 1].to_csv("LOG0_spC%s.CSV" % index, sep=",", index=False)
